cc75c6915ecfa6992621d5d48a67adb1cee668d3
edmirsuljic <edmir_2@live.se>
2020-01-28 09:27:57 +0100

    added some stuff

--- work/report/js/main.js
+++ work/report/js/main.js
@@ -1,10 +1,36 @@
 /**
  * A function to wrap it all in.
  */
-(function () {
-    "use strict";
+function getData() {
+    'use strict';
 
-
-
+    fetch('data/schools_malmo.json')
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        })
     console.log("All ready.");
-}());
+}
+
+function updateTable(){
+    var jsonData = getData()
+    console.log(jsonData)
+    var tableHTML = "<tr>";
+    for (var headers in jsonData[0]) {
+      tableHTML += "<th>" + headers + "</th>";
+    }
+    tableHTML += "</tr>";
+  
+    for (var eachItem in jsonData) {
+      tableHTML += "<tr>";
+      var dataObj = jsonData[eachItem];
+      for (var eachValue in dataObj){
+        tableHTML += "<td>" + dataObj[eachValue] + "</td>";
+      }
+      tableHTML += "</tr>";
+    }
+  
+    document.getElementById('school-table').innerHTML = tableHTML;
+  }
